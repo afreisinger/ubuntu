92fc2cce9ec7b5866f92b102c461be154d0c9e66
Adrian Freisinger <afreisinger@gmail.com>
2023-05-20 14:47:33 -0300

    fix project path

--- bin/set_enviroment.sh
+++ bin/set_enviroment.sh
@@ -2,7 +2,7 @@
 
 export CI_REGISTRY=registry.gitlab.com    #The address of the GitLab Container Registry. Only available if the Container Registry is enabled for the project. This variable includes a :port value if one is specified in the registry configuration.
 export CI_PROJECT_NAMESPACE=afreisinger   #The project namespace (username or group name) of the job.
-export CI_PROJECT_NAME=ubuntu-sshd      #The name of the directory for the project. For example if the project URL is gitlab.example.com/group-name/project-1, CI_PROJECT_NAME is project-1.
+export CI_PROJECT_NAME=ubuntu      #The name of the directory for the project. For example if the project URL is gitlab.example.com/group-name/project-1, CI_PROJECT_NAME is project-1.
 export CI_PROJECT_PATH=$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME  #The project namespace with the project name included.
 export CI_REGISTRY_IMAGE=$CI_REGISTRY/$CI_PROJECT_PATH  #The address of the project’s Container Registry. Only available if the Container Registry is enabled for the project.
 export DOCKER_REGISTRY=docker.io
